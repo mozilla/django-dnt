8c09045304af8e1f444efd0f521a4251d9ada44c
James Socol <james@mozilla.com>
2011-02-16 13:26:45 -0500

    Set up setup.py.

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name='django-dnt',
+    version='0.1.0',
+    description='Make Django requests aware of the DNT header.',
+    long_description=open('README.rst').read(),
+    author='James Socol',
+    author_email='james@mozilla.com',
+    url='http://github.com/mozilla/django-dnt',
+    license='BSD',
+    packages=['django_dnt'],
+    include_package_data=True,
+    package_data = { '': ['README.rst'] },
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Environment :: Web Environment :: Mozilla',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
